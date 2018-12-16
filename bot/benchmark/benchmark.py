6fe47570a32d677dcd2e464c5be76737baf79de4
Alberto Rota <alberto.rota.jimenez@gmail.com>
2018-12-16 21:25:59 +0100

    Changed "benchmark" to run in parallel

--- bot/benchmark/benchmark.py
+++ bot/benchmark/benchmark.py
@@ -1,5 +1,5 @@
-import sys
 import time
+import multiprocessing as mp
 from collections import Counter
 
 import numpy as np
@@ -15,41 +15,59 @@ class Benchmark(object):
         accumulated_moves = 0
         max_tiles = Counter()
         runs = 0
-        secs = 0
+        real_secs = 0
+        game_secs = 0
 
-        while runs < max_runs and secs <= max_secs:
-            board = Board(np.array([
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0]]),
-                initialize=True
-            )
+        pool = mp.Pool(mp.cpu_count())
+        while runs < max_runs and real_secs <= max_secs:
+            real_start = time.time()
+            results = [pool.apply_async(Benchmark.__run_game__, args=(ai,)) for _ in range(mp.cpu_count())]
+            outputs = [p.get() for p in results]
+            real_secs += time.time() - real_start
 
-            start_time = time.time()
-            num_of_movements = 0
-            while not board.is_game_over:
-                direction = ai.get_next_move(board)
-                board = board.swipe_grid(direction, spawn_tile=True)
-                num_of_movements += 1
+            for output in outputs:
+                board, num_of_movements, elapsed_time = output
 
-            runs += 1
-            secs += time.time() - start_time
-            accumulated_score += board.score
-            accumulated_moves += num_of_movements
-            max_tiles[max(map(max, board.grid))] += 1
+                runs += 1
+                game_secs += elapsed_time
+                accumulated_score += board.score
+                accumulated_moves += num_of_movements
+
+                max_tiles[max(map(max, board.grid))] += 1
+
             print("\r", end="", flush=True)
             print("Played {} games, {:4.2f}%, ETA: {} seconds)".format(
                 runs,
-                100 * runs/max_runs,
-                int((secs / runs) * (max_runs - runs))
+                100 * runs / max_runs,
+                int(((game_secs / runs) * (max_runs - runs)) / mp.cpu_count())
             ), end="", flush=True)
 
         print("Runs: " + str(runs))
-        print("Secs: " + str(secs))
+        print("Real secs: " + str(real_secs))
+        print("Game secs: " + str(game_secs))
         print("Avg score: " + str(accumulated_score / runs))
         print("Avg moves: " + str(accumulated_moves / runs))
-        print("Moves per sec: " + str(accumulated_moves / secs))
+        print("Moves per sec: " + str(accumulated_moves / game_secs))
         print("Max tile distribution: ")
-        for number, times in max_tiles.items():
+        for number, times in sorted(max_tiles.items()):
             print(str(number) + ": " + str(times))
+
+    @staticmethod
+    def __run_game__(ai: AiAbc) -> (Board, int, float):
+        board = Board(np.array([
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]),
+            initialize=True
+        )
+
+        start_time = time.time()
+        num_of_movements = 0
+
+        while not board.is_game_over:
+            direction = ai.get_next_move(board)
+            board = board.swipe_grid(direction, spawn_tile=True)
+            num_of_movements += 1
+
+        return board, num_of_movements, time.time() - start_time
