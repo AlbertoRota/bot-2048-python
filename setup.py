9eb38730fb8c261e26e937d1ace85cc328dd5a57
Alberto Rota <alberto.rota.jimenez@gmail.com>
2019-01-14 22:48:41 +0100

    Re-added compilation step 2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-# from distutils.core import setup
-# from Cython.Build import cythonize
-#
-# setup(
-#     ext_modules=cythonize("bot/helloworld.pyx", annotate=True)
-# )
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize("bot/helloworld.pyx", annotate=True)
+)
